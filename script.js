60db685626bd5e873db26708e0e3bf115aff25fd
unknown <helenvcolon@gmail.com>
2023-10-19 00:01:06 -0400

    created js, html and css files with starting codes.

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Define Variables */
+
+var startBtn = document.querySelector("#startbutton");
+var startPg = document.querySelector("startpage");
+var questPg = document.querySelector("questpage")
